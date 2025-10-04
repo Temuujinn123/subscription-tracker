ebd5e1aa61790dbd61f4608f81cd2c1f9332383c
Temuujinn123 <temuujinn8563@gmail.com>
2025-10-03 22:38:41 -0400

    Added auth google debug print

--- back-end/internal/handlers/auth.go
+++ back-end/internal/handlers/auth.go
@@ -114,9 +114,15 @@ func AuthGoogle(db models.Database, googleOauthConfig *oauth2.Config) http.Handl
 			return
 		}
 
+		println("req.Code ===> ", req.Code)
+
 		// Exchange authorization code for tokens
 		googleToken, err := googleOauthConfig.Exchange(context.Background(), req.Code)
+
+		println("googleToken ===> ", googleToken)
+
 		if err != nil {
+			println(&err)
 			http.Error(w, "Failed to exchange token", http.StatusBadRequest)
 			return
 		}
