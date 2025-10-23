e7c83ecc029792f58bc955c9f07d56d1a7f0d0e2
Temuujinn123 <temuujinn8563@gmail.com>
2025-10-23 18:26:43 -0400

    Added some seo configs

--- front-end/components/Layout.tsx
+++ front-end/components/Layout.tsx
@@ -1,6 +1,7 @@
 // components/Layout.tsx
 import { ReactNode } from "react";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,6 +10,14 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+      <Head>
+        <title>SubTrack</title>
+        <meta
+          name="description"
+          content="Subscripion tracker web-app for everyone"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main className="container mx-auto px-4 py-8">{children}</main>
     </div>
